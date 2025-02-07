4434913a365f47a20a62db762f12df9c7ae42ccd
Tatiana <tatianalimongi@gmail.com>
2025-02-07 20:27:59 -0300

    first commit

---
+++ src/app/PasswordChecker.ts
@@ -0,0 +1,46 @@
+/*
+    A password is invalid if:
+    - length is less than 8 chars
+    - has no upper case letter
+    - has no lower case letter
+
+    Requirement 2:
+    - return the reasons that make a password invalid
+
+    Requirement 3:
+    - refactor
+    - Admin password should also contain a number
+*/
+
+export enum PasswordErrors {
+    SHORT = 'Password is too short!',
+    NO_UPPER_CASE = 'Upper case letter required!',
+    NO_LOWER_CASE = 'Lower case letter required!'
+}
+
+export interface CheckResult {
+    valid: boolean,
+    reasons: PasswordErrors[]
+}
+
+export class PasswordChecker {
+    public checkPassword(password: string): CheckResult{
+
+        const reasons:PasswordErrors[] = [];
+
+        if (password.length < 8) {
+            reasons.push(PasswordErrors.SHORT);
+        }
+        if (password == password.toLowerCase()) {
+            reasons.push(PasswordErrors.NO_UPPER_CASE);
+        }
+        if (password == password.toUpperCase()) {
+            reasons.push(PasswordErrors.NO_LOWER_CASE
+            );
+        }
+        return {
+            valid: reasons.length > 0 ? false : true,
+            reasons: reasons
+        };
+    }
+}
---
+++ src/test/PasswordChecker.test.ts
@@ -0,0 +1,48 @@
+import { PasswordChecker, PasswordErrors } from "../app/PasswordChecker"
+
+describe('PasswordChecker test suite', ()=> {
+    
+    let sut: PasswordChecker;
+
+    beforeEach(()=> {
+        sut = new PasswordChecker();
+    })
+
+    it('Password with less than 8 chars is invalid', ()=>{
+        const actual = sut.checkPassword('1234567');
+        expect(actual.valid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.SHORT);
+    });
+
+    it('Password with more than 8 chars is valid', ()=>{
+        const actual = sut.checkPassword('12345678');
+        expect(actual.reasons).not.toContain(PasswordErrors.SHORT)
+    });
+
+    it('Password with no upper case letter is invalid', ()=>{
+        const actual = sut.checkPassword('abcd');
+        expect(actual.valid).toBe(false);
+        expect(actual.reasons).toContain(PasswordErrors.NO_UPPER_CASE)
+    });
+
+    it('Password with upper case letter is valid', ()=>{
+        const actual = sut.checkPassword('abcD');
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_UPPER_CASE)
+    });
+
+    it('Password with no lower case letter is invalid', ()=>{
+        const actual = sut.checkPassword('ABCD');
+        expect(actual.reasons).toContain(PasswordErrors.NO_LOWER_CASE)
+    });
+
+    it('Password with lower case letter is valid', ()=>{
+        const actual = sut.checkPassword('ABCd');
+        expect(actual.reasons).not.toContain(PasswordErrors.NO_LOWER_CASE)
+    });
+
+    it('Complex password is valid', ()=>{
+        const actual = sut.checkPassword('1234ABCd');
+        expect(actual.reasons).toHaveLength(0);
+        expect(actual.valid).toBe(true);
+    });
+})
